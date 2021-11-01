3e8714d617b4758b8ea612c80b4c1c9d59f20677
Igor Patrick Ponticelli <patrickigor.ip@gmail.com>
2021-11-01 10:20:55 -0300

    Add app Moveit
    App criada com react.js e next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownContextProvider } from '../contexts/CountdownContext';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ínicio | move.it</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <CountdownContextProvider>
+        <section>
+          <div>
+            <Profile/>
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownContextProvider>
+    </div>
+  )
+}
